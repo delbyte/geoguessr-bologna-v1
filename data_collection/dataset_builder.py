a57ccd30c3576051b678f116d9e20c6112e3538e
delbyte <blazed0utcode@gmail.com>
2025-03-20 23:38:50 +0530

    add dataset builder to convert JSON data to CSV format

---
+++ data_collection/dataset_builder.py
@@ -0,0 +1,21 @@
+import json
+import csv
+
+json_file = "dataset/unnamed.json"
+csv_file = "dataset/finaldata.csv"
+image_folder = "dataset/images_resized"
+
+# load JSON data
+with open(json_file, "r") as f:
+    data = json.load(f)
+
+# write data in csv
+with open(csv_file, "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow(["image_id", "latitude", "longitude"])  # CSV header
+
+    for image_id, coords in data.items():
+        image_path = f"{image_folder}/{image_id}.jpg"
+        writer.writerow([image_path, coords[0], coords[1]]) 
+
+print(f"CSV saved at {csv_file}")
