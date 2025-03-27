2b5ce82bad37d95ddb0f597ee6b6d2710aea92e1
delbyte <blazed0utcode@gmail.com>
2025-03-27 16:59:33 +0530

    fix import paths for ResNet and MobileNet geolocation models

--- model/train.py
+++ model/train.py
@@ -2,8 +2,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from architectures.resnet import ResNetGeolocation
-from architectures.mobilenet import MobileNetGeolocation
+from model.architectures.resnet import ResNetGeolocation
+from model.architectures.mobilenet import MobileNetGeolocation
 from model.utils.dataloader import GeolocationDataset, transform
 import os
 
